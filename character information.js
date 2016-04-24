874496f8d9de7c6c5ec50addf9cefd75ba6283fc
peiminz <peiminzhong@hotmail.com>
2016-04-24 01:18:51 -0400

    create demo of graph3
    right now i am using this certain js format data and use a function to
    create svg everytime it get a movie string

---
+++ character information.js
@@ -0,0 +1,28 @@
+var character={
+  "Superman": {
+    "APPEARANCES":2496,
+    "Year":1986,
+    "pic":"image/dc/superman.jpg"
+    
+  },
+  "Super Girl": {
+    "APPEARANCES":3,
+    "Year":1972,
+    "pic":"image/dc/supergirl.jpg"
+  },
+  "Swamp Thing": {
+    "APPEARANCES":309,
+    "Year":1972,
+    "pic":"image/dc/swamp.jpg"
+  },
+  "Captain America": {
+    "APPEARANCES":3360,
+    "Year":1940,
+    "pic":"image/marvel/captainamerica.jpg"
+  },
+  "Red Skull": {
+    "APPEARANCES":10,
+    "Year":1965,
+    "pic":"image/marvel/redskull.jpg"
+  }
+}
---
+++ character.js
@@ -0,0 +1,23 @@
+var movie={
+  "Superman II": {
+    "COMPANY":"DC",
+    "RELEASE":1980,
+    "Characters":["Superman"]
+    
+  },
+  "Supergirl": {
+    "COMPANY":"DC",
+    "RELEASE":1984,
+    "Characters":["Super Girl"]
+  },
+  "The Return of Swamp Thing": {
+    "COMPANY":"DC",
+    "RELEASE":1989,
+    "Characters":["Swamp Thing"]
+  },
+  "Captain America": {
+    "COMPANY":"Marvel",
+    "RELEASE":1990,
+    "Characters":["Captain America","Red Skull"]
+  }
+}
