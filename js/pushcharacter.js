503ca1a1381f9b84489c243615b4423ccc2ead98
peiminz <peiminzhong@hotmail.com>
2016-04-26 17:30:10 -0400

    changed 3rd graph

--- js/pushcharacter.js
+++ js/pushcharacter.js
@@ -1,6 +1,6 @@
 function gethero(chararr) {
     d3.selectAll(".character").remove();
-    height = 400;
+    height = 450;
     width = 370;
     padding = 20;
     for (m = 0; m < chararr.length; m++) {
@@ -30,7 +30,6 @@ function gethero(chararr) {
 
             }
             var svg = d3.select("#photo" + m)
-                // .attr("class","character")
                 .append("svg")
                 .attr("class", "character")
                 .attr("width", width)
@@ -43,38 +42,72 @@ function gethero(chararr) {
                 .attr("class", "image")
                 .attr("x", 15)
                 .attr("y", 10)
-                .attr("width", 150)
-                .attr("height", 150)
+                .attr("width", 120)
+                .attr("height", 120)
                 .attr("xlink:href", image)
                 .attr("opacity", 0.9);
 
             svg.append("text")
                 .attr("x", 15)
-                .attr("y", 180)
+                .attr("y", 150)
                 .text("Name: " + name)
                 .style("fill", "#BBBBBB");
 
-            lScale = d3.scale.linear().domain([0, 4050]).range([0, 150]);
+            lScale = d3.scale.linear().domain([0, 4050]).range([0, 350]);
+            comicscale=d3.scale.linear().domain([0, 4043]).range([0, 350]);
+            moviescale=d3.scale.linear().domain([0, 8]).range([0, 350]);
+            salescale=d3.scale.linear().domain([0, 1575847963]).range([0, 350]);
 
             svg.append("text")
                 .attr("x", 15)
-                .attr("y", 200)
+                .attr("y", 165)
                 .text("Appearances in comics: " + appearance + " times")
                 .style("fill", "#BBBBBB");
 
+
             svg.append("text")
                 .attr("x", 15)
-                .attr("y", 220)
+                .attr("y", 195)
                 .text("Appearances in movie: " + more.length + " times")
                 .style("fill", "#BBBBBB");
+
+            svg.append("rect")
+                .attr("x", 15)
+                .attr("y", 175)
+                .attr("width", comicscale(appearance))
+                .attr("height",8)
+                .style("fill","#990033")
+                .style("opacity",0.6);
+
+            svg.append("rect")
+                .attr("x", 15)
+                .attr("y", 200)
+                .attr("width", moviescale(more.length))
+                .attr("height",8)
+                .style("fill","#990033")
+                .style("opacity",0.6);
 
             for (k = 0; k < more.length; k++) {
                 svg.append("text")
                     .attr("x", 15)
-                    .attr("y", 240 + k * 20)
+                    .attr("y", 225 + k * 25)
                     .text(more[k].film + ": $" + more[k].revenue)
                     .style("fill","#BBBBBB");
+
+                svg.append("rect")
+                .attr("x", 15)
+                .attr("y", 230+k *25)
+                .attr("width", salescale(more[k].revenue))
+                .attr("height",8)
+                .style("fill","#ff4c4c")
+                .style("opacity",0.6);
+
+
+
+
             }
         }
     }
 }
+
+gethero([["Batman"],["Iron Man"]]);
