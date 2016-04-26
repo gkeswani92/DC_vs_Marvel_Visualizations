ddd589103d34b551eff23534436fb6e4ae409899
peiminz <peiminzhong@hotmail.com>
2016-04-25 22:21:56 -0400

    update 3rd graph

--- js/pushcharacter.js
+++ js/pushcharacter.js
@@ -1,7 +1,7 @@
 function gethero(chararr){
         d3.selectAll(".character").remove();
-        height=300;
-        width=300;
+        height=400;
+        width=370;
         padding=20;
         for (m=0;m<chararr.length;m++){
         // film=moviearr[m]
@@ -19,9 +19,9 @@ function gethero(chararr){
             // console.log(movie[j].Characters)
 
             if(MovieJSON[j].Characters.indexOf(name) != -1) {
-                
+                film=MovieJSON[j].FILM
                 revenue= MovieJSON[j].ADJUSTED;
-                each={film:j,revenue:revenue};
+                each={film:film,revenue:revenue};
                 more.push(each)
             }
         
@@ -38,34 +38,34 @@ function gethero(chararr){
             area.enter()
             .append("svg:image")
             .attr("class","image")
-            .attr("x",35)
-            .attr("y",20)
+            .attr("x",15)
+            .attr("y",10)
             .attr("width",150)
             .attr("height",150)
             .attr("xlink:href",image)
             .attr("opacity",0.9);
  
             svg.append("text")
-            .attr("x",35)
-            .attr("y",200)
+            .attr("x",15)
+            .attr("y",180)
             .text("Name: "+name);
 
             lScale=d3.scale.linear().domain([0,4050]).range([0,150]);
 
             svg.append("text")
-            .attr("x",35)
-            .attr("y",220)
-            .text("Appearances in comic: "+appearance+" times");
+            .attr("x",15)
+            .attr("y",200)
+            .text("Appearances in comics: "+appearance+" times");
 
             svg.append("text")
-            .attr("x",35)
-            .attr("y",240)
+            .attr("x",15)
+            .attr("y",220)
             .text("Appearances in movie: "+more.length+" times");
 
             for (k=0;k<more.length;k++){
                 svg.append("text")
-            .attr("x",35)
-            .attr("y",260+k*20)
+            .attr("x",15)
+            .attr("y",240+k*20)
             .text(more[k].film+": $"+more[k].revenue);
 
 
